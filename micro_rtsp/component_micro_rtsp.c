fb4b0af1bcacf5ce8ec7e6e9816765dcc64c4ca6
Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>
2019-12-27 23:17:48 +0100

    initial files to make lib usable by esp-idf >=4.0 projects
    Signed-off-by: Bartlomiej Cieszkowski <bartlomiej.cieszkowski@gmail.com>

---
+++ micro_rtsp/component_micro_rtsp.c
@@ -0,0 +1,10 @@
+
+void register_micro_rtsp(void)
+{
+	/* 1. Create all necessary things in this function
+	 * 2. Wrap around funcionality from "main" loop into the function
+	 * 3. Create a classic FreeRTOS task
+	 */
+
+}
+
---
+++ micro_rtsp/component_micro_rtsp.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void register_micro_rtsp(void);;
+
+#ifdef __cplusplus
+}
+#endif
